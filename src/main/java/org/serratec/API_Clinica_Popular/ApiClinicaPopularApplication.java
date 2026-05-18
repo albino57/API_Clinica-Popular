b4cfb5898d3b9b25320ae44adb017ed7e06b2d70
Rafael Albino <ansdles@live.com>
2026-05-18 16:13:52 -0300

    feat: subindo arquivos basicos

---
+++ src/main/java/org/serratec/API_Clinica_Popular/ApiClinicaPopularApplication.java
@@ -0,0 +1,13 @@
+package org.serratec.API_Clinica_Popular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiClinicaPopularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiClinicaPopularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/serratec/API_Clinica_Popular/ApiClinicaPopularApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.API_Clinica_Popular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClinicaPopularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
